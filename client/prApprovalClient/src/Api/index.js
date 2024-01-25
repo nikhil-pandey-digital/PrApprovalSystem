da4424d0b9acf793367d9dd963212065761d2d23
nikhil pandey <105156966+nikhil-pandey-digital@users.noreply.github.com>
2024-01-25 18:06:14 +0530

    Updated  base url of deployed backend

--- client/prApprovalClient/src/Api/index.js
+++ client/prApprovalClient/src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: https://pr-approval-system-api.vercel.app",
+  baseURL: "https://pr-approval-system-api.vercel.app",
 });
 
 API.interceptors.request.use((req) => {
