6ce5e96bde92430513184eaa7041b8d00cb49aaa
nikhil pandey <105156966+nikhil-pandey-digital@users.noreply.github.com>
2024-01-25 19:15:58 +0530

    Update cors for allowing everydomain

--- server/app.js
+++ server/app.js
@@ -8,10 +8,11 @@ const app = express();
 // app.use(cors());
 
 const corsOptions = {
-  origin: "https://pr-approval-system-client.vercel.app",
+  origin: function (origin, callback) {
+    callback(null, true);
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
-  
 };
 
 app.use(cors(corsOptions));
